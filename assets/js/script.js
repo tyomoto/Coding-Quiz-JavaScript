85d41d3862788a511cf5d4d30d265116f1706a78
T Omoto <tyler.omoto@gmail.com>
2022-05-25 01:38:04 -0400

    adding basic css and var for js

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// Timer
+var timerEl = document.querySelector(".timer-count");
+var timer;
+var timerCount;
+
+// start button
+var start= document.querySelector(".start-button");
+
+// Starter Instructions
+var starterIns= document.querySelector("#starter-box");
+
+// Questions
+var questionsEl= document.querySelector("#questions-container");
+var question= document.querySelector("#question");
+var questionCount = 0;
+
+// Answers
+var answer1btn = document.querySelector("#answer-1");
+var answer2btn = document.querySelector("#answer-2");
+var answer3btn = document.querySelector("#answer-3");
+var answer4btn = document.querySelector("#answer-4");
+
+// Final Score Screen + Submitting Score
+var finalscore = document.querySelector("#final-score");
+var initialsinput = document.querySelector(".initials-text");
+var submitScoreButton = document.querySelector("#submit-score");
+
+
+// High Scores
+var highScoresEl = document.querySelector("#high-scores-container");
+var scoreListEl = document.querySelector("#high-score-list");
+var scoreList = [];
+var clearScoreButton = document.querySelector("#clear-scores");
+var goBackButton = document.querySelector("#back-to-game");
+
+
+// Begin quiz 
+function startQuiz(){
+
+}
+
+
+// Event listeners
